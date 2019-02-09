2da6a585dfa4ca22b40d7e7700b125868c0732b1
Akshay <akshayyadav7613@gmail.com>
2019-02-09 22:26:46 +0530

    Initial commit......

---
+++ app/src/main/java/com/sharepreferance/MainActivity.java
@@ -0,0 +1,19 @@
+package com.sharepreferance;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        SharedPreferences sharedPreferences= this.getSharedPreferences("com.sharepreferance",Context.MODE_PRIVATE);
+        
+    }
+}
